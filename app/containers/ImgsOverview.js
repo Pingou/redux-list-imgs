cccda618eb063c75135f260ae9b3a53e442e4839
pingou <minipingou@gmail.com>
2018-09-05 14:08:49 +0200

    removed comments

--- app/containers/ImgsOverview.js
+++ app/containers/ImgsOverview.js
@@ -6,37 +6,7 @@ import Orientation from "react-native-orientation";
 import { initOrientation } from "../actions/orientation";
 
 class ImgsOverview extends Component {
-     _nbColumns = 1;
 
-/*
-
-    _orientationDidChange = orientation => {
-        this.setNbColumns(orientation);
-        alert('changed')
-    };
-
-    componentWillMount() {
-        this.setNbColumns(Orientation.getInitialOrientation());
-        
-    }
-    componentWillUnmount() {
-        Orientation.removeOrientationListener(this._orientationDidChange);
-    }
-
-    componentDidMount() {
-        this.props.fetchData("/imgs");
-        Orientation.addOrientationListener(this._orientationDidChange);
-    }
-
-
-    setNbColumns(orientation) {
-        if (orientation === "LANDSCAPE") {
-            this._nbColumns = 2;
-        } else {
-            this._nbColumns = 1;
-        }
-    }
-*/
     getNbColumns() {
         if (this.props.orientation === "LANDSCAPE") {
            return 2;
@@ -45,14 +15,11 @@ class ImgsOverview extends Component {
         }
     }
 
-
-
-    componentWillMount() {
+   componentWillMount() {
         this.props.initOrientation();
-    }
+    } 
     componentDidMount() {
         this.props.fetchData("/imgs");
-      
     }
 
     onImgSelected(img) {
